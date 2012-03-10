f2261a0fb98baae5c5233c6e785d2e3b73482440
Jakk Wong <jakk.wong@gmail.com>
2012-03-10 04:43:07 -0800

    Data model generated.

---
+++ socialrank_project/socialrank_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Page(models.Model): 
+    name = models.CharField(max_length=100)
+    link = models.URLField()
+#    pic = models.ImageField()
+
+class Friends(models.Model):
+    page = models.ForeignKey(Page)
+    following = models.IntegerField()
+    followers = models.IntegerField()
+    date = models.DateField()
+    
+#class Upload_SeedPages(models.Model):
+#    seedpage_file = models.FileField()
+#    seedpage_link = models.URLField()
+    
+
